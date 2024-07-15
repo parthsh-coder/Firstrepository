e5166d6c2954087eeaea14e6fdfc45c0550ef671
Parth sharma <parthsharma4556@gmail.com>
2024-07-15 12:19:03 +0530

    Add files via upload

---
+++ arr1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int arr[10];
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		printf("enter any numbers:");
+		scanf("%d",&arr[i]);
+		
+	}
+	printf("arr=%d",arr[i]);
+	return 0;
+}
---
+++ array pre defined.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int arr[5]={1,2,3,5,3};
+	int i;
+	for(i=0;i<=10;i++)
+	{
+		printf("%d\n",arr[i]);
+	}
+	return 0;
+}
